87a56a208daef7725b5a225183578514c8f5300f
zelaz23 <150788403+zelaz23@users.noreply.github.com>
2024-10-28 12:46:56 +0100

    Add files via upload

--- app_offshore_logistic.py
+++ app_offshore_logistic.py
@@ -233,5 +233,5 @@ if st.sidebar.button('CALCULATE BEST OPTION', key="calculate_option_button"):
         st.write('Anual expected CO2 emissions:')
         st.metric(label="CO2 EMISSIONS", value = heli_emissions) """
 
-else:
-    st.write('DEFINE THE PARAMETERS TO ANALIZE AND CLIC IN CALCULATE')
+""" else:
+    st.write('DEFINE THE PARAMETERS TO ANALIZE AND CLIC IN CALCULATE') """
