cb68a7c85a9f4a48f6a7d265ee99aa090ac02cdc
zelaz23 <150788403+zelaz23@users.noreply.github.com>
2024-10-30 10:08:06 +0100

    Add files via upload

--- app_offshore_logistic.py
+++ app_offshore_logistic.py
@@ -41,7 +41,7 @@ with st.sidebar:
 
     # Desplegable de opciones
     selected_option_lat_OSW = st.selectbox('Select latitude or type one: ', options=list(lat_OSW_options.keys()))
-
+    st.write("Select:", selected_option_lat_OSW)
     # Determina el valor de latitud según la opción seleccionada
     if "Other" in selected_option_lat_OSW:
         # Si selecciona "Other", habilitar el número manual
